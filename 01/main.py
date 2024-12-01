6e6dfdca852f869e53308eb071ee77761fcf291b
aoc-penguin-25 <hmtkty+aoc2025@gmail.com>
2024-11-30 23:16:54 -0800

    more pylint fixes

--- 01/main.py
+++ 01/main.py
@@ -58,16 +58,16 @@ def part2():
     right_counter: Counter[int] = Counter(right_list)
 
     # init a sum
-    sum: int = 0
+    sum_so_far: int = 0
 
     # go thru everything in left list
     for entry in left_counter:
         # add up the value of the number * occurance in left * occurance in right
-        sum += entry * left_counter[entry] * right_counter[entry]
+        sum_so_far += entry * left_counter[entry] * right_counter[entry]
 
     # no need to go thru right as well, since it would be *0 with left.
 
-    print("part 2 is: ", sum)
+    print("part 2 is: ", sum_so_far)
 
 
 part1()
