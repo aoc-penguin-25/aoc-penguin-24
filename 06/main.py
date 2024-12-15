99dea8ce92ec11d5cce0040cd87c44b57c6f270c
aoc-penguin-25 <hmtkty+aoc2025@gmail.com>
2024-12-14 23:54:19 -0800

    rename directions

--- 06/main.py
+++ 06/main.py
@@ -1,12 +1,24 @@
 import itertools
 
-directions = itertools.cycle([[-1,0],[0,1],[1,0],[0,-1]])
-facing: list[int] = next(directions)
+face_to_vector = {
+    '^': (-1,0),
+    '>': (0,1),
+    'v': (1,0),
+    '<': (0,-1)
+}
+
+turn_right = {
+    '<': '^',
+    '^': '>',
+    '>': 'v',
+    'v': '<'
+}
 
 cur_row = 0
 cur_col = 0
 
 with open("06/input.txt", encoding="utf-8") as f:
+    facing = '^'
     grid_strings = f.read().splitlines()
     for row_num in range(len(grid_strings)):
         # huh, I hardcoded starting facing north. /shrug.
@@ -20,22 +32,22 @@ with open("06/input.txt", encoding="utf-8") as f:
     for row in grid_strings:
         grid.append(list(row))
     grid[cur_row][cur_col] = 'X'
-    next_row: int = cur_row + facing[0]
-    next_col: int = cur_col + facing[1]
+    next_row: int = cur_row + face_to_vector[facing][0]
+    next_col: int = cur_col + face_to_vector[facing][1]
     while(next_row < len(grid[0]) and next_row >= 0 and next_col < len(grid) and next_col >= 0):
         # leave footsteps
         # print(cur_row, cur_col)
         if grid[next_row][next_col] == '#':
             # turn right
-            facing = next(directions)
+            facing = turn_right[facing]
         else:
             # step forward
             cur_row = next_row
             cur_col = next_col
             # leave footsteps
             grid[cur_row][cur_col] = 'X'
-        next_row = cur_row + facing[0]
-        next_col = cur_col + facing[1]
+        next_row = cur_row + face_to_vector[facing][0]
+        next_col = cur_col + face_to_vector[facing][1]
     # we left the grid, count it
     footsteps = 0
     for row in grid:
